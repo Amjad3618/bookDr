9b015858dba949b4bd7bbfd2ff1c0d409cdc4e08
Amjad3618 <amjadalimazari06@gmail.com>
2026-04-22 16:10:44 +0500

    firebase is connected to user app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
-import 'package:bookdr/widgets/bottom_nav_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'routes/app_routes.dart';
 import 'views/login/login_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // options: const FirebaseOptions(
+    //   apiKey: "AIzaSyA8n9sXo2l3m1a5b6c7d8e9f0g1h2i3j4k5l6m7n8o9p0q1r2s3t4u5v6w7x8y9z0",
+    //   appId: "1:1234567890:android:abcdef1234567890abcdef1234567890",
+    //   messagingSenderId: "1234567890",
+    //   projectId: "bookdr-12345",
+    // ),
+  );
   runApp(const MyApp());
 }
 
